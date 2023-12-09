2efa5dd51cc9abff9eba1ffd27d4b4130a1ecb08
Damla Irmak Doğdu <idogdu60@gmail.com>
2023-12-09 03:04:17 +0300

    oha site :3

---
+++ themeToggle.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggleBtn = document.getElementById('themeToggleBtn');
+    const burnEyesText = document.getElementById('burnEyesText');
+    const clickSound = document.getElementById('clickSound');
+
+    themeToggleBtn.addEventListener('click', function () {
+        const isSwitchingToDarkTheme = document.body.classList.contains('light-theme');
+
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
+        themeToggleBtn.classList.toggle('clicked');
+
+        if (isSwitchingToDarkTheme) {
+            clickSound.play();
+        }
+        const isLightTheme = document.body.classList.contains('light-theme');
+        burnEyesText.textContent = isLightTheme ? 'AAAAAAAAAAAAAAAA' : 'click here if youre a masochist';
+    });
+});
